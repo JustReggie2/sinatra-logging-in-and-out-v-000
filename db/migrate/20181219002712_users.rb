bae2b0c0b351142a343e43164fa3696e86e3d84f
Reginald T Carter iI <reginaldcarterii@gmail.com>
2018-12-19 20:27:43 +0000

    Automatically backed up by Learn

--- db/migrate/20181219002712_users.rb
+++ db/migrate/20181219002712_users.rb
@@ -3,7 +3,7 @@ class Users < ActiveRecord::Migration[5.1]
     create_table :users do |u|
       u.string :username
       u.string :password
-      u.decimal :balance
+      u.float :balance
     end
   end
 end
