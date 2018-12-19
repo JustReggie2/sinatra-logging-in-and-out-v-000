87b53372ef270b55ed57841e52611e5bebcef6ff
Reginald T Carter iI <reginaldcarterii@gmail.com>
2018-12-19 20:55:35 +0000

    Automatically backed up by Learn

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,13 +1,12 @@
 class Helpers
 
   def self.current_user(session)
-    binding.pry
     @user = User.find(session[:user_id])
     @user
   end
 
   def self.is_logged_in?(session)
-    @user.id == session[:user_id]
+    !!session[:user_id]
   end
 
 end
